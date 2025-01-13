88e6a0215329a4a83b3c021caac4c1b10415ee0c
arun.linux.gateway <arun.kk1984@gmail.com>
2025-01-13 22:15:41 +0530

    parameterization of connection string amd container creation

--- blob_util.py
+++ blob_util.py
@@ -1,34 +1,51 @@
+import os
 import json
 import asyncio
-from azure.identity import DefaultAzureCredential
+from dotenv import load_dotenv
 from azure.storage.blob import BlobServiceClient
 from azure.iot.device import IoTHubModuleClient
 from azure.core.exceptions import ResourceExistsError
 
-CONNECTION_STRING = "DefaultEndpointsProtocol=http;BlobEndpoint=http://storage_module:11002/edgestore;AccountName=edgestore;AccountKey=iYMKd+VQXJDxNwGInLuzl9tA5oUlxTcZnIVhfcGoUnRkXgGl7CROA7fYjOvXd+qFulujnhqjsdtYZpfNHqAVPg==;"
+#load_dotenv("/config/config.env")
+CAM_ID = "cam-02" # os.environ.get("CAMERA_NAME")
+#BLOB_MODULE = "192.168.29.245"
+BLOB_MODULE = "storage_module"
+STORAGE_ACCOUNT_NAME = "edgestore"
+STORAGE_ACCOUNT_KEY = "7Cyf5F4Arlo96IKsm+eWsw=="
+CONNECTION_STRING = f"DefaultEndpointsProtocol=http;BlobEndpoint=http://{BLOB_MODULE}:11002/{STORAGE_ACCOUNT_NAME};AccountName={STORAGE_ACCOUNT_NAME};AccountKey={STORAGE_ACCOUNT_KEY};"
+print(f"Connecting to Edge blob account {STORAGE_ACCOUNT_NAME} with {CONNECTION_STRING}")
 INPUT_FOLDER = "/store"
-
-account_url = "https://<storageaccountname>.blob.core.windows.net"
-default_credential = DefaultAzureCredential()
-
-# Create the BlobServiceClient object
-blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+try:
+    blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING, api_version="2019-12-12")
+    print("Blob service client created")
+except Exception as e:
+    print(f"Blob service client creation error: {e}")
 
 # Create a unique name for the container
-container_name = "edgestore"
+image_container_name = f"{CAM_ID}-imagestore"
+video_container_name = f"{CAM_ID}-videostore"
 
-# Create the container
 try:
-    container_client = blob_service_client.create_container(container_name)
+    # Create image container
+    container_client = blob_service_client.create_container(image_container_name)
+    print(f"Container {image_container_name} created")
 except ResourceExistsError:
-    print('A container with this name already exists')
+    print(f"A container with name {image_container_name} already exists")
     pass
 
+try:
+    # Create video container
+    container_client = blob_service_client.create_container(video_container_name)
+    print(f"Container {video_container_name} created")
+except ResourceExistsError:
+    print(f"A container with name {video_container_name} already exists")
+    pass 
 
-async def handle_input(message):
+async def handle_message(message):
     try:
         message_text = message.data.decode("utf-8")
         message_json = json.loads(message_text)
+        print(message_json)
         if message.input_name == "image_blob_trigger":
             file_name = message_json["alert_image"]
             alert_id = message_json["alert_id"],
@@ -36,28 +53,51 @@ async def handle_input(message):
             cam_id = message_json["cam_id"]
             alert_day = message_json["alert_day"]
             blob_name = f"{alert_id}-{cam_id}-{alert_day}-{frame_id}.jpg"
+
+            try:
+                # Create a blob client using the local file name as the name for the blob
+                blob_client = blob_service_client.get_blob_client(container=image_container_name, blob=blob_name)
+            except Exception as e:
+                print(f"Blob client creations error: {e}")
+
         elif message.input_name == "video_blob_trigger":
             file_name = message_json["alert_video"]
             alert_id = message_json["alert_id"]
             cam_id = message_json["cam_id"]
             alert_day = message_json["alert_day"]
             blob_name = f"{alert_id}-{cam_id}-{alert_day}.avi"
-    
-        # Create a blob client using the local file name as the name for the blob
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-        print(f"Uploading to Azure Storage as blob: {file_name}")
-        
+            
+            try:
+                # Create a blob client using the local file name as the name for the blob
+                blob_client = blob_service_client.get_blob_client(container=video_container_name, blob=blob_name)
+            except Exception as e:
+                print(f"Blob client creations error: {e}")
+
+        else:
+            print("Unknown Input Stream")
+
         # Upload the created file
-        with open(file=file_name, mode="rb") as data:
-            await blob_client.upload_blob(data, overwrite=True)
+        if file_name:
+            print(f"Uploading to Azure Cloud Storage as blob: {file_name}")
+            with open(file=file_name, mode="rb") as data:
+                await blob_client.upload_blob(data, overwrite=True)
+                print(f"{file_name} added to container")
 
     except Exception as e:
         print(f"Error processing input message: {e}")
 
 async def main():
-    module_client = IoTHubModuleClient.create_from_edge_environment()
-    await module_client.connect()
-    module_client.on_message_received = handle_input
+
+
+    #connection_string = "HostName=WaterUtilityHub.azure-devices.net;DeviceId=dot-edge-vision2;SharedAccessKey=BYNmMKdeQUscnzAFRHPciWX1A1I7cfGHcNRW3zBvDfw="
+    try:
+        module_client = IoTHubModuleClient.create_from_edge_environment()
+        #module_client = IoTHubModuleClient.create_from_connection_string(connection_string)
+        module_client.connect()
+    except Exception as e:
+        print(f"Module Connection Error: {e}")
+
+    module_client.on_message_received = handle_message
 
     while True:
         await asyncio.sleep(100)
--- dockerfile
+++ dockerfile
@@ -1,6 +1,5 @@
 FROM python:3.10-slim AS base
 
-#RUN apt update && apt install -y libsm6 libxrender1 libxext6 libgl1 libglib2.0-0 libxcb-xinerama0
 WORKDIR /app
 COPY . .
 
@@ -8,6 +7,6 @@ RUN --mount=type=cache,target=/root/.cache/pip \
     --mount=type=bind,source=requirements.txt,target=/app/requirements.txt \
     python -m pip install -r /app/requirements.txt
 
-EXPOSE 11002
+#EXPOSE 11002
 
 ENTRYPOINT [ "python", "/app/blob_util.py"]
